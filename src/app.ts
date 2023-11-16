5a39b85fc93c06bca58fe248ab59bad4f0c983c6
교헌 이 <ghl12078@gmail.com>
2023-11-16 10:34:15 +0900

    Feat: Add limit option

--- src/app.ts
+++ src/app.ts
@@ -35,8 +35,8 @@ mongoose
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(bodyParser.urlencoded({ limit: "2mb", extended: true }));
 
 app.use(lusca.xssProtection(true));
 app.use(cors());
